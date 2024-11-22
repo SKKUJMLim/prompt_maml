94757c3520f4db2c90a72653ec5a339d084c3801
SKKUJMLim <skku.jmlim@gmail.com>
2024-11-22 17:50:58 +0900

    Arbiter 구현

---
+++ Arbiter.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# VAE 모델 정의
+class VAE(nn.Module):
+    def __init__(self, input_dim=3*84*84, hidden_dim=400, latent_dim=20):
+        super(VAE, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc21 = nn.Linear(hidden_dim, latent_dim)  # Mean
+        self.fc22 = nn.Linear(hidden_dim, latent_dim)  # Log variance
+        self.fc3 = nn.Linear(latent_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, input_dim)
+
+    def encode(self, x):
+        h1 = torch.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        h3 = torch.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 3*32*32))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
--- inner_loop_optimizers.py
+++ inner_loop_optimizers.py
@@ -56,12 +56,12 @@ class GradientDescentLearningRule(nn.Module):
             for key in names_weights_dict.keys():
                 if key in 'linear':
                     # classifier의 learning rate를 0.0으로 하여 freeze한다
-                    updated_names_weights_dict[key] = names_weights_dict[key] - self.learning_rate * names_grads_wrt_params_dict[key]
+                    updated_names_weights_dict[key] = names_weights_dict[key] - freeze_layer_step_size * names_grads_wrt_params_dict[key]
                 else:
                     updated_names_weights_dict[key] = names_weights_dict[key] - freeze_layer_step_size * names_grads_wrt_params_dict[key]
 
             for key in prompted_weights_dict.keys():
-                updated_prompt_weights_dict[key] = prompted_weights_dict[key] - self.learning_rate * prompted_grads_wrt_params_dict[key]
+                updated_prompt_weights_dict[key] = prompted_weights_dict[key] - 50.0 * prompted_grads_wrt_params_dict[key]
         else:
             ## MAML
             for key in names_weights_dict.keys():
