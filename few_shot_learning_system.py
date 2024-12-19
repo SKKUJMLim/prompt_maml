d3b5f4ed5f395eec87ac1442d9999273974b8c91
SKKUJMLim <skku.jmlim@gmail.com>
2024-12-19 17:08:11 +0900

    task acc

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -9,6 +9,8 @@ import torch.optim as optim
 from meta_neural_network_architectures import VGGReLUNormNetwork, ResNet12
 from inner_loop_optimizers import GradientDescentLearningRule, LSLRGradientDescentLearningRule
 
+from utils.storage import save_statistics
+
 
 def set_torch_seed(seed):
     """
@@ -38,8 +40,8 @@ class MAMLFewShotClassifier(nn.Module):
         self.use_cuda = args.use_cuda
         self.im_shape = im_shape
         self.current_epoch = 0
-
         self.rng = set_torch_seed(seed=args.seed)
+        self.csv_exist= True
 
         if self.args.backbone == 'ResNet12':
             self.classifier = ResNet12(im_shape=self.im_shape, num_output_classes=self.args.
@@ -247,6 +249,8 @@ class MAMLFewShotClassifier(nn.Module):
         total_target_accuracies = [[] for i in range(num_steps)]
         per_task_target_preds = [[] for i in range(len(x_target_set))]
 
+        information = {}
+
         if torch.cuda.device_count() > 1:
             self.classifier.module.zero_grad()
         else:
@@ -285,6 +289,7 @@ class MAMLFewShotClassifier(nn.Module):
             y_support_set_task = y_support_set_task.view(-1)
             x_target_set_task = x_target_set_task.view(-1, c, h, w)
             y_target_set_task = y_target_set_task.view(-1)
+
 
             for num_step in range(num_steps):
 
@@ -335,6 +340,30 @@ class MAMLFewShotClassifier(nn.Module):
             task_losses = torch.sum(torch.stack(task_losses))
             total_losses.append(task_losses)
             total_accuracies.extend(accuracy)
+
+            if current_iter == 'test':
+                information={}
+                information['phase'] = current_iter
+                information['task_idx'] = task_id
+                information['accuracy'] = accuracy.mean().item()
+                # .mean()은 True 값의 비율을 반환. 이는 정확도를 의미
+
+                if os.path.exists(self.args.experiment_name + '/' + self.args.experiment_name + "_per_task_acc.csv"):
+                    self.csv_exist = False
+
+                if self.csv_exist:
+                    save_statistics(experiment_name=self.args.experiment_name,
+                                    line_to_add=list(information.keys()),
+                                    filename=self.args.experiment_name + "_per_task_acc.csv", create=True)
+                    self.csv_exist = False
+                    save_statistics(experiment_name=self.args.experiment_name,
+                                    line_to_add=list(information.values()),
+                                    filename=self.args.experiment_name + "_per_task_acc.csv", create=False)
+                else:
+                    save_statistics(experiment_name=self.args.experiment_name,
+                                    line_to_add=list(information.values()),
+                                    filename=self.args.experiment_name + "_per_task_acc.csv", create=False)
+
 
             if not training_phase:
                 if torch.cuda.device_count() > 1:
