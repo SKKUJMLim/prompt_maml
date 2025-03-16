5f91899a2f0f1e0c36d0e974efb9b6930032daaa
SKKUJMLim <skku.jmlim@gmail.com>
2025-03-16 14:06:00 +0900

    step

--- few_shot_learning_system_Arbiter.py
+++ few_shot_learning_system_Arbiter.py
@@ -313,14 +313,14 @@ class MAMLFewShotClassifier(nn.Module):
 
                 grads, context_grads = gradients[:-1], gradients[-1]
 
-                z = z - self.args.text_embedding_learning_rate * context_grads
+                # z = z - self.args.text_embedding_learning_rate * context_grads
 
-                # if self.args.learnable_per_layer_per_step_inner_loop_learning_rate:
-                #     z = z - self.task_embedding_adaptive_learning_rate[num_step] * context_grads
-                #     z = z - self.args.text_embedding_learning_rate * context_grads
-                # else:
-                #     z = z - self.args.text_embedding_learning_rate * context_grads
-                #     z = (1 - self.args.init_inner_loop_weight_decay * self.args.text_embedding_learning_rate) * z - self.args.text_embedding_learning_rate * context_grads
+                if self.args.learnable_per_layer_per_step_inner_loop_learning_rate:
+                    z = z - self.task_embedding_adaptive_learning_rate[num_step] * context_grads
+                    # z = z - self.args.text_embedding_learning_rate * context_grads
+                else:
+                    z = z - self.args.text_embedding_learning_rate * context_grads
+                    # z = (1 - self.args.init_inner_loop_weight_decay * self.args.text_embedding_learning_rate) * z - self.args.text_embedding_learning_rate * context_grads
 
                 names_weights_copy = self.apply_inner_loop_update(loss=support_loss,
                                                                   names_weights_copy=names_weights_copy,
