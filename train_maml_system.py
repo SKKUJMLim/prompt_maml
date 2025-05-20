f5d39c073dfb2c4b51a1b3c3ece205c7b0a6c2b1
SKKUJMLim <skku.jmlim@gmail.com>
2025-05-20 11:10:33 +0900

    mixup

--- train_maml_system.py
+++ train_maml_system.py
@@ -31,7 +31,7 @@ from multiprocessing import freeze_support
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_1shot_filter64_miniImagenet.json --gpu_to_use 0
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_5shot_filter64_miniImagenet.json --gpu_to_use 0
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_1shot_filter64_tieredImagenet.json --gpu_to_use 0
-# python3 train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_5shot_filter64_tieredImagenet.json --gpu_to_use 0
+# python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_5shot_filter64_tieredImagenet.json --gpu_to_use 0
 
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_1shot_filter64_CIFAR_FS.json --gpu_to_use 0
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_5shot_filter64_CIFAR_FS.json --gpu_to_use 0
