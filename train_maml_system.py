200d705afa88d31f4b1cd6af8ea7d7d9aa44d003
SKKUJMLim <skku.jmlim@gmail.com>
2025-06-06 13:35:29 +0900

    weight decay

--- train_maml_system.py
+++ train_maml_system.py
@@ -37,8 +37,8 @@ from multiprocessing import freeze_support
 # python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_padding_5way_5shot_Resnet12_tieredImagenet.json --gpu_to_use 0
 
 ## (Fixed patch & Random patch)
-# python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_fixed_patch_5way_5shot_filter64_miniImagenet.json --gpu_to_use 0
-# python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/MAML_Prompt_random_patch_5way_5shot_filter64_miniImagenet.json --gpu_to_use 0
+# python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/per_template/MAML_Prompt_random_patch_5way_5shot_filter128_miniImagenet.json --gpu_to_use 0
+# python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/per_template/MAML_Prompt_fixed_patch_5way_5shot_filter128_miniImagenet.json --gpu_to_use 0
 
 '''[Resnet12] MAML+Prompt experiment'''
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Prompt/Resnet12/MAML_Prompt_padding_5way_1shot_Resnet12_miniImagenet.json --gpu_to_use 0
