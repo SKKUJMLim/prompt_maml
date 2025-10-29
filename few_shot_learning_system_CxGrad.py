d7e6e1eeb790f08ab3726e38928a9975bf629dc4
SKKUJMLim <skku.jmlim@gmail.com>
2025-10-29 16:33:54 +0900

    Noisy

--- few_shot_learning_system_CxGrad.py
+++ few_shot_learning_system_CxGrad.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 from meta_neural_network_architectures import VGGReLUNormNetwork
-from inner_loop_optimizers_CxGrad import GradientDescentLearningRule, LSLRGradientDescentLearningRule
+from inner_loop_optimizers import GradientDescentLearningRule, LSLRGradientDescentLearningRule
 from utils.storage import save_statistics
 
 
@@ -389,7 +389,7 @@ class MAMLFewShotClassifier(nn.Module):
         :param num_step: An integer indicating the number of the step in the inner loop.
         :return: the crossentropy losses with respect to the given y, the predictions of the base model.
         """
-        preds, _ = self.classifier.forward(x=x,
+        preds = self.classifier.forward(x=x,
                                         params=weights,
                                         training=training,
                                         backup_running_statistics=backup_running_statistics,
--- train_maml_system.py
+++ train_maml_system.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     elif any(tag in args.experiment_name for tag in ('MeTAL',)):
         from few_shot_learning_system_MeTAL import MAMLFewShotClassifier
     elif any(tag in args.experiment_name for tag in ('CxGrad',)):
-        from few_shot_learning_system_ALFA import MAMLFewShotClassifier
+        from few_shot_learning_system_CxGrad import MAMLFewShotClassifier
     elif any(tag in args.experiment_name for tag in ('GAP',)):
         from few_shot_learning_system_GAP import MAMLFewShotClassifier
 
